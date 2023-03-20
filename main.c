2eb6df71bb4ff95eb5eb88140da3c06b876cdd98
Arnaud BEAUDUIN <abeaudui@e2r10p20.clusters.42paris.fr>
2023-03-20 15:23:49 +0100

    test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abeaudui <abeaudui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 11:13:29 by arnaud            #+#    #+#             */
-/*   Updated: 2023/03/19 19:54:34 by abeaudui         ###   ########.fr       */
+/*   Updated: 2023/03/20 15:20:02 by abeaudui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,35 +33,37 @@ int main(int ac, char **av)
 	i = 0;
 	while(i < ac - 1) 
 	{
-		push_number(&pile_a, tab[i++]);	
+		push_number(&pile_a, tab[i++]);
 	}
 	i = 0;
+	push_a(&pile_a, &pile_b);
+	push_a(&pile_a, &pile_b);
+	//push_a(&pile_a, &pile_b);
+	//push_a(&pile_a, &pile_b);
+	//push_a(&pile_a, &pile_b);
+	//push_a(&pile_a, &pile_b);
+	//push_a(&pile_a, &pile_b);
+	//push_a(&pile_a, &pile_b);
+	//push_a(&pile_a, &pile_b);
+	//push_a(&pile_a, &pile_b);
+	//rotate(&pile_a);
+	reverse_rotate_b(&pile_b);
+	//rrr(&pile_a, &pile_b);
+	//ss(&pile_a, &pile_b);
 	
-
-	push_a(&pile_a, &pile_b);
-	push_a(&pile_a, &pile_b);
-	push_a(&pile_a, &pile_b);
-	push_a(&pile_a, &pile_b);
-	push_a(&pile_a, &pile_b);
-	push_a(&pile_a, &pile_b);
-	push_a(&pile_a, &pile_b);
-	push_a(&pile_a, &pile_b);
-	push_a(&pile_a, &pile_b);
-
-	
-
-
 	printf("/ PILE A /\n");
 	while (pile_a != NULL)
 	{
-		printf("%d\n", pile_a->content);
+		printf("| %d |", pile_a->content);
+		//printf("| %d |", pile_a->index);
+
 		pile_a = pile_a ->next;
 	}
 	
-	printf("/ PILE B /\n");
+	printf("\n/ PILE B /\n");
 	while (pile_b != NULL)
 	{
-		printf("%d\n", pile_b->content);
+		printf("| %d |", pile_b->content);
 		pile_b = pile_b->next;
 	}
 
--- push.c
+++ push.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abeaudui <abeaudui@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dsenatus <dsenatus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 17:31:32 by abeaudui          #+#    #+#             */
-/*   Updated: 2023/03/19 19:54:27 by abeaudui         ###   ########.fr       */
+/*   Updated: 2023/03/20 14:04:45 by dsenatus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,40 @@ void push_a(t_pile **pile_a, t_pile **pile_b)
 	t_pile *temp;
 	temp = *pile_a;
 	
+	if(temp->next == NULL)
+	{
+		push_number(pile_b, temp->content);
+		free(temp);
+		*pile_a = NULL;
+		return ;
+	}
 	while (temp->next->next != NULL)
 		temp = temp->next;
-	
 	t_pile *second_to_last = temp;
 	t_pile *last = temp->next;
 	second_to_last->next = NULL;
 	
 	push_number(pile_b, last->content);
+}
+
+void push_b(t_pile **pile_a, t_pile **pile_b)
+{
+	
+	t_pile *temp;
+	temp = *pile_b;
+	
+	if(temp->next == NULL)
+	{
+		push_number(pile_a, temp->content);
+		free(temp);
+		*pile_b = NULL;
+		return ;
+	}
+	while (temp->next->next != NULL)
+		temp = temp->next;
+	t_pile *second_to_last = temp;
+	t_pile *last = temp->next;
+	second_to_last->next = NULL;
+	
+	push_number(pile_a, last->content);
 }
